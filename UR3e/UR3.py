b7419d621a5eef9cb4af06fd1e4cfda8e841d218
elenaalegret <elenaalegret@gmail.com>
2024-06-19 12:28:03 +0200

    Update the UR3 file

--- UR3e/UR3.py
+++ UR3e/UR3.py
@@ -1,14 +1,16 @@
 """
-This Python script establishes a socket connection to a UR robot controller, sends joint path commands to move the 
+This Python script establishes a socket connection to a UR robot controller, sends joint path commands to move the
 robot along specified trajectories, and executes scripts to open and close the gripper. It demonstrates how to control
-a UR robot programmatically via socket communication.
+a UR robot programmatically via socket communication. The joint paths are read from an XML file using a custom
+function from the 'conf_extraction' module.
 """
+
 import socket
 import time
-from Code.list_transformation import list_transformation
+from conf_extraction import read_file
 
 # Robot UR's IP address
-HOST = "10.10.73.23X"
+HOST = "10.10.73.236"
 
 # Port of the server on the robot
 PORT = 30002
@@ -20,8 +22,8 @@ Cerrar_pinza = 'pinza10UR3.py'
 def send_joint_path(path, sock):
     """
     Send a joint path to the robot controller to move along specified configurations.
-        :param path: A list of joint configurations (each configuration is a list of joint angles in radians).
-        :param sock: The socket object for communication with the robot controller.
+    :param path: A list of joint configurations (each configuration is a list of joint angles in radians).
+    :param sock: The socket object for communication with the robot controller.
     """
     for joint_config in path:
         print(joint_config)
@@ -32,32 +34,45 @@ def send_joint_path(path, sock):
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
-# Trajectory -- configurations (joint variables in radians)
-path1, path2, path3 = read_file()
+# Read joint paths from the XML file
+paths = read_file()
 
-# Send the trajectory to the robot controller
-send_joint_path(path, sock)
+# Truncate paths to include only the first six joint angles
+paths = [[i[:6] for i in path] for path in paths]
 
-# Send script file to close the gripper
+# Send the joint paths to the robot controller and the script file to close the gripper
+send_joint_path(path[0], sock)
 with open(Cerrar_pinza, 'rb') as f:
     sock.sendall(f.read())
 time.sleep(1)
 
-# Send the trajectory to the robot controller
-send_joint_path(path, sock)
 
-# Send script file to open the gripper
+send_joint_path(path[1], sock)
 with open(Abrir_pinza, 'rb') as f:
     sock.sendall(f.read())
 time.sleep(1)
 
-# Send the trajectory to the robot controller
-send_joint_path(path, sock)
+# Send the joint paths to the robot controller
+send_joint_path(path[2], sock)
+send_joint_path(path[3], sock)
+with open(Cerrar_pinza, 'rb') as f:
+    sock.sendall(f.read())
+time.sleep(1)
+
+send_joint_path(path[4], sock)
+send_joint_path(path[5], sock)
+
+with open(Abrir_pinza, 'rb') as f:
+    sock.sendall(f.read())
+time.sleep(1)
+
+# Send a single joint configuration to the robot controller
+final_path=[[0.8567222222, 0.3292777778, 0.6716666667, 0.2431944444, 0.2560277778, 0.3501388889]]
+send_joint_path(final_path, sock)
 
 # Message printed when the trajectory execution is completed
 print("Trayectoria finalizada")
-
 data = sock.recv(1024)
 
-# Close the connection
+# Close connection
 sock.close()
